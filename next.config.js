31f990c056e626f6750157cbb095f81624bea357
ArrreMayuGaming <mayureshshitole@gmail.com>
2021-07-19 13:49:35 +0530

    image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["i.ytimg.com", "yt3.ggpht.com","https://youtube.com"],
+    domains: ["i.ytimg.com", "yt3.ggpht.com", "https://youtube.com"],
+    loader: "imgix",
+    path: "",
   },
 };
